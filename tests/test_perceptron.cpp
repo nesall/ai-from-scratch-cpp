4acb0e4752b3fad2b7629d3db1962dbec1969e10
Arman Sahakyan <arman_sahakyan@hotmail.com>
2025-07-26 17:02:34 +0400

    Added Perceptron

---
+++ tests/test_perceptron.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "models/perceptron.hpp"
+
+void test_gate(const std::string &gate_name, const std::vector<std::vector<float>> &inputs,
+  const std::vector<int> &targets) {
+  int expected_accuracy = targets.size();
+  models::Perceptron p(2, 0.1f, 20); // 2-input perceptron
+  p.fit(inputs, targets);
+
+  int correct = 0;
+  for (size_t i = 0; i < inputs.size(); ++i) {
+    int prediction = p.predict(inputs[i]);
+    if (prediction == targets[i]) {
+      ++correct;
+    }
+  }
+  std::cout << "[Perceptron] ";
+  std::cout << gate_name << ": " << (correct >= expected_accuracy ? "PASSED" : "FAILED") << std::endl;
+}
+
+void test_perceptron() {
+  std::vector<std::vector<float>> X = {
+      {0, 0},
+      {0, 1},
+      {1, 0},
+      {1, 1}
+  };
+
+  std::vector<int> y_and = { 0, 0, 0, 1 };
+  std::vector<int> y_or = { 0, 1, 1, 1 };
+
+  test_gate("AND Gate", X, y_and);
+  test_gate("OR Gate", X, y_or);
+}
